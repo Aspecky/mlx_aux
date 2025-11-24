064ff82aa5eeab3afc5522d0bd807533ab1f78e0
Aspecky <tarrihmoad@proton.com>
2025-11-24 23:37:36 +0100

    Update vector2 and ivector2 unit functions to return magnitude

--- source/Vector2/unit.c
+++ source/Vector2/unit.c
@@ -6,29 +6,33 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 17:56:06 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/11/22 19:44:28 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/11/24 23:37:06 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Vector2.h"
 #include <math.h>
 
-t_vector2	vector2_unit(t_vector2 a)
+t_vector2	vector2_unit(t_vector2 a, double *magnitude)
 {
-	double	magnitude;
+	double	mag;
 
-	magnitude = sqrt(a.x * a.x + a.y * a.y);
-	if (magnitude == 0)
+	mag = sqrt(a.x * a.x + a.y * a.y);
+	if (magnitude)
+		*magnitude = mag;
+	if (mag == 0)
 		return ((t_vector2){0, 0});
-	return ((t_vector2){a.x/magnitude, a.y/magnitude});
+	return ((t_vector2){a.x/mag, a.y/mag});
 }
 
-t_ivector2	ivector2_unit(t_ivector2 a)
+t_ivector2	ivector2_unit(t_ivector2 a, double *magnitude)
 {
-	double	magnitude;
+	double	mag;
 
-	magnitude = sqrt(a.x * a.x + a.y * a.y);
-	if (magnitude == 0)
+	mag = sqrt(a.x * a.x + a.y * a.y);
+	if (magnitude)
+		*magnitude = mag;
+	if (mag == 0)
 		return ((t_ivector2){0, 0});
-	return ((t_ivector2){(int)(a.x/magnitude), (int)(a.y/magnitude)});
+	return ((t_ivector2){(int)(a.x/mag), (int)(a.y/mag)});
 }
