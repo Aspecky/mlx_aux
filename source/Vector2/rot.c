6385a2e9d7bb7869fb58280b4cfbc508e0724990
Aspecky <tarrihmoad@proton.com>
2025-12-01 16:23:58 +0100

    Add optimization to vector rotation

--- source/Vector2/rot.c
+++ source/Vector2/rot.c
@@ -6,21 +6,33 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 00:35:14 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/11/22 19:44:28 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:23:10 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Vector2.h"
 #include <math.h>
 
-t_vector2 vector2_rot(t_vector2 a, double tetha)
+t_vector2 vector2_rot(t_vector2 a, double theta)
 {
-	return ((t_vector2){a.x * cos(tetha) - a.y * sin(tetha),
-						a.x * sin(tetha) + a.y * cos(tetha)});
+	if (theta == -M_PI_2)
+		return ((t_vector2){a.y, -a.x});
+	if (theta == M_PI_2)
+		return ((t_vector2){-a.y, a.x});
+	if (theta == M_PI)
+		return ((t_vector2){-a.x, -a.y});
+	return ((t_vector2){a.x * cos(theta) - a.y * sin(theta),
+						a.x * sin(theta) + a.y * cos(theta)});
 }
 
-t_ivector2 ivector2_rot(t_ivector2 a, double tetha)
+t_ivector2 ivector2_rot(t_ivector2 a, double theta)
 {
-	return ((t_ivector2){(int)(a.x * cos(tetha) - a.y * sin(tetha)),
-						(int)(a.x * sin(tetha) + a.y * cos(tetha))});
+	if (theta == -M_PI_2)
+		return ((t_ivector2){a.y, -a.x});
+	if (theta == M_PI_2)
+		return ((t_ivector2){-a.y, a.x});
+	if (theta == M_PI)
+		return ((t_ivector2){-a.x, -a.y});
+	return ((t_ivector2){(int)(a.x * cos(theta) - a.y * sin(theta)),
+						(int)(a.x * sin(theta) + a.y * cos(theta))});
 }
--- source/include/Vector2.h
+++ source/include/Vector2.h
@@ -6,7 +6,7 @@
 /*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 21:19:22 by mtarrih           #+#    #+#             */
-/*   Updated: 2025/11/30 18:13:11 by mtarrih          ###   ########.fr       */
+/*   Updated: 2025/12/01 16:03:21 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_vector2	vector2_add(t_vector2 a, t_vector2 b);
 t_vector2	vector2_sub(t_vector2 a, t_vector2 b);
 t_vector2	vector2_mul(t_vector2 a, t_vector2 b);
 t_vector2	vector2_scale(t_vector2 a, double x);
-t_vector2	vector2_rot(t_vector2 a, double tetha);
+t_vector2	vector2_rot(t_vector2 a, double theta);
 t_vector2	vector2_unit(t_vector2 a);
 double		vector2_mag(t_vector2 a);
 
@@ -37,7 +37,7 @@ t_ivector2	ivector2_add(t_ivector2 a, t_ivector2 b);
 t_ivector2	ivector2_sub(t_ivector2 a, t_ivector2 b);
 t_ivector2	ivector2_mul(t_ivector2 a, t_ivector2 b);
 t_ivector2	ivector2_scale(t_ivector2 a, int x);
-t_ivector2	ivector2_rot(t_ivector2 a, double tetha);
+t_ivector2	ivector2_rot(t_ivector2 a, double theta);
 t_ivector2	ivector2_unit(t_ivector2 a);
 double		ivector2_mag(t_ivector2 a);
 
